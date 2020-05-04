5b65861f7e6f2bbe53ab73f15c96ceb564a05fd8
Paula <paulacorcuera2@gmail.com>
2020-05-04 19:00:40 +0200

    terminando ejercicio 3

--- script2.sh
+++ script2.sh
@@ -1,5 +1,21 @@
-for i in $(cat procesos.txt | awk '{prinnt $2}'); do
+read -p "Introduce un valor de PID: " v1
+contador=0
 
-echo $i
+for i in $(cat procesos.txt | awk '{print $2}') ;do
+
+if [ $v1 -lt $i ] 
+then
+contador=$((contador=+1))
+
+else
+
+echo "no hay ningun PID"
+exit
+
+$i
+
+fi
 
 done
+
+echo "Hay $contador procesos mayores que el PID"
